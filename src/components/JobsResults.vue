280493f6ad3e235535e4e1aa5da54f4cec6fd4bb
Akanksh Chitimalla <144524423+akanksh-ch@users.noreply.github.com>
2025-06-19 12:29:07 +0530

    Added styles to anchor tag

--- src/components/JobsResults.vue
+++ src/components/JobsResults.vue
@@ -3,9 +3,10 @@
 </script>
 
 <template>
-  <div :class="wrapper">
+  <div class="wrapper">
     <div class="job" v-if="jobs.length > 0" v-for="job in jobs" :key="job.title">
-      {{ job }}
+      <strong><a :href=job.url>{{ job.title }}</a></strong> - <strong>{{ job.company }}</strong>
+      <p>{{ job.location }}</p>
     </div>  
   </div>
 </template>
@@ -21,5 +22,10 @@
     border-radius: 0.25rem;
     margin: 1rem;
   }
-
+  a {
+    text-decoration: none;
+  }
+  a:link {
+    color: hsl(220, 100%, 50%) 
+  }
 </style>
