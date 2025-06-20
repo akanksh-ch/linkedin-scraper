5b7373cf202e6cf2c4550bf3b22a935b10c1380f
Akanksh Chitimalla <144524423+akanksh-ch@users.noreply.github.com>
2025-06-20 12:54:51 +0530

    removed dummy job listing

--- src/components/JobsPage.vue
+++ src/components/JobsPage.vue
@@ -4,7 +4,7 @@
     import { ref } from 'vue';
     
     const jobs = ref([
-        {title: 'Software Testing Engineer', company: 'Dell', location: 'Birmingham, United Kingdom', url: 'https://example.com'}
+        //{title: 'Software Testing Engineer', company: 'Dell', location: 'Birmingham, United Kingdom', url: 'https://example.com'}
     ])
 </script>
 
