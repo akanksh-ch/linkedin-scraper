0cde1873d042864084a73b5d9cc7bb77bfa01fae
Akanksh Chitimalla <144524423+akanksh-ch@users.noreply.github.com>
2025-07-01 13:50:58 +0530

    Loading animation not quite working

--- src/components/LoadingAnimation.vue
+++ src/components/LoadingAnimation.vue
@@ -1,15 +1,32 @@
 <script setup>
+    import { ref, onMounted, onBeforeUnmount } from 'vue'
     const loading = defineModel('loading', {type: Boolean})
+    const index = ref(0)
+    const intervalID = ref(0)
+    
+    /*
+    onMounted(() => {
+        intervalID.value = setInterval(() => {
+        */
+            document.querySelector('p').innerText = "Loading".slice(0, index) + '.' + "Loading".slice(index + 1)
+            index.value += 1
+            if (index.value > "Loading".length) {
+                index.value = 0
+            }
+            /*
+        }, 100)
+    })
+    */
+    
+    //onBeforeUnmount(() => clearInterval(intervalID))
 </script>
 
 <template>
-<p>Loading</p>
+<p></p>    
 
 <!--Debug elements-->
-<!--
 <p>state: {{ loading }}</p>
 <button @click="loading = loading ? false : true">Flip state</button>
--->
 </template>
 
 <style scoped>
