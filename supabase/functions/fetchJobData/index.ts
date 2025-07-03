99969d8bb45d885d6fd716ac3a888c97af474041
Akanksh Chitimalla <144524423+akanksh-ch@users.noreply.github.com>
2025-07-03 12:39:45 +0530

    Using || instead of ?? to catch empty strings as well

--- supabase/functions/fetchJobData/index.ts
+++ supabase/functions/fetchJobData/index.ts
@@ -49,7 +49,7 @@ Deno.serve(async (req) => {
     // Job description
     const jobsDescFetched = await(await fetch(jobsDescUrl)).text();
     const jobsDescDoc = new DOMParser().parseFromString(jobsDescFetched, 'text/html');
-    info.description = jobsDescDoc.querySelector('.show-more-less-html')?.innerText ?? 'No job description found';
+    info.description = jobsDescDoc.querySelector('.show-more-less-html')?.innerText || 'No job description found';
 
     // to get rid of show more/less and remove excess white space and newlines
     for (const [key, value] of Object.entries(info)) {
